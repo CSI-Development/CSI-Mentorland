e2e7465b5e150138cb7e4fd2941a2e71ff59e843
shashankpatil125 <shashankpatil125@gmail.com>
2024-03-13 19:42:28 +0530

    fix: build fix

---
+++ src/components/student/dashboardStudentComponents/wishlist/Wishlist.Component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Breadcrumb from "../Breadcrumb.Component";
+
+function Wishlist() {
+  return (
+    <div className="text-black w-full ">
+      <Breadcrumb titel="Notification" />
+    </div>
+  );
+}
+
+export default Wishlist;

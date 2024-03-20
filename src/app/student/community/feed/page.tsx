5d7992b5c97a0302423c86b13ef9666eccffa04f
Shivam Gavandi <shivamgavandi256@gmail.com>
2024-03-20 18:57:40 +0530

    feat-created community feed page file

---
+++ src/app/student/community/feed/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CommunityFeed = () => {
+  return (
+    <div className="w-full h-screen bg-white text-black">
+      <div>banner</div>
+      <div>
+        <div>action buttons</div>
+        <div>main section</div>
+        <div>right content</div>
+      </div>
+    </div>
+  );
+};
+
+export default CommunityFeed;
